6ec16a145a42272b0d69419b56aeb619e19c667f
Josh LaFrance <joshua@joshualafrance.com>
2021-12-28 16:36:41 -0500

    Added Naughty Attributes

--- Assets/Scripts/CardData.cs
+++ Assets/Scripts/CardData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 [CreateAssetMenu]
 public class CardData : ScriptableObject
@@ -8,13 +9,13 @@ public class CardData : ScriptableObject
    public string CardName;
    public int CardCost;
    public int CardLevel;
-   public string CardDesc;
    public int CardDurability;
-   public enum CardColor { Red, Blue, Green};
-   public enum CardType { Attack, Skill, Power};
-   public enum CardRarity { Common, Uncommon, Rare, Legendary}
-   public CardColor cColor;
-   public CardType cType;
-   public CardRarity cRarity;
-
+   public enum CColor { Red, Blue, Green};
+   public enum CType { Attack, Skill, Power};
+   public enum CRarity { Common, Uncommon, Rare, Legendary}
+   public CColor CardColor;
+   public CType CardType;
+   public CRarity CardRarity;
+    [ResizableTextArea]
+    public string CardDesc;
 }
